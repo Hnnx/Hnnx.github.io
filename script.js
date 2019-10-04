92f3178672ac90124e4a99183d8c8c463498033a
Anh <anhedoniic@hotmail.com>
2019-10-04 16:24:21 +0200

    Commiting new(old repo) files to sln/GIT

---
+++ script.js
@@ -0,0 +1,14 @@
+//obsolete, replaced with jQuery
+function removeOk(){
+
+    var getFooter = document.getElementById("remove");
+
+    if (getFooter.style.display === "none") {
+        getFooter.style.display = "block";
+    }
+    else {
+        getFooter.style.display = "none";
+    }
+
+}
+
