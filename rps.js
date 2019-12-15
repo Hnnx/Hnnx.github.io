2bf6f2c394f805b331f045e97e6ef1c6976a5207
Nejc <nejc.stibilj01@gmail.com>
2019-12-15 22:31:39 +0100

    setInterval RDM

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
 console.log("handshake"); 
+
+
+let result = document.getElementById("result");
+
+let getRDM = () => {
+    result.innerHTML = Math.floor((Math.random() * 9 + 1));
+}
+
+
+setInterval(getRDM, 500);
