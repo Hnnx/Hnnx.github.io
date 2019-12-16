95db009848f03296962b71eb9d5eea051e67dab6
Nejc <nejc.stibilj01@gmail.com>
2019-12-16 16:02:02 +0100

    adding more files

--- RPS/rps.js
+++ RPS/rps.js
@@ -1,13 +1,31 @@
-console.log("handshake"); 
-
-
 let result = document.getElementById("result");
 
-let getRDM = () => {
-    result.innerHTML = Math.floor((Math.random() * 9 + 1));
+let rdm = top => {
+    result.innerHTML = Math.floor((Math.random() * top + 1));
 }
 
-setInterval(getRDM, 700);
+
+let clickRandom = document.getElementById("placeholder");
+
+function getRDM(top) {
+
+    clickRandom.innerHTML = "Your random number is: " + Math.floor(Math.random() * top + 1);
+}
+
+let autoRandom = document.getElementById("autoPlaceholder");
+
+
+function autoRDM(top) {
+    autoRandom.innerHTML = "yr rdm is: " + Math.floor((Math.random() * top + 1));    
+}
+
+function rr() {
+    let x = Math.floor((Math.random() * 20 + 1));
+}
+
+setInterval(rr, 200);
+
+
 
 
 
